8db47195860d6b68559dc32770823c4953028b78
GuillaumeBarreau <guillaume.barreau@zenika.com>
2020-10-29 08:36:47 +0100

    ✨ Adding Button components

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
-import { ExampleComponent } from 'react_component_library'
+import {
+  Wefight_ExampleComponent,
+  Wefight_ProgressBar,
+  Wefight_Button
+} from 'react_component_library'
+
 import 'react_component_library/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+
+  const [completed, setCompleted] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => setCompleted(Math.floor(Math.random() * 100) + 1), 2000);
+  }, []);
+
+  return (
+  <>
+    <Wefight_ExampleComponent text="Create React Library Example 😄" />
+    <Wefight_ProgressBar bgcolor={"#6a1b9a"} completed={completed} />
+    <Wefight_Button />
+  </>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
-import styles from './styles.module.css'
 
-export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
-}
+import {ExampleComponent} from './ExampleComponent'
+import {ProgressBar} from './ProgressBar'
+import {Button} from './Button'
+
+export const Wefight_ExampleComponent = (props) => <ExampleComponent {...props}></ExampleComponent>
+export const Wefight_ProgressBar = (props) => <ProgressBar {...props}></ProgressBar>
+export const Wefight_Button = (props) => <Button {...props}></Button>
