5a8a29c396d3066778172c53d7589224432cfad3
GuillaumeBarreau <guillaume.barreau@zenika.com>
2020-10-29 08:47:18 +0100

    Updates

---
+++ precache-manifest.e3150f513f16436f0aecee35842886a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d375bc812d1d957d021eeeff4054711",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4d35fa3c3bfb6a03f2a8",
+    "url": "./static/css/main.bc83bb68.chunk.css"
+  },
+  {
+    "revision": "52e6dea13891c6999b43",
+    "url": "./static/js/2.692fcd46.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.692fcd46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d35fa3c3bfb6a03f2a8",
+    "url": "./static/js/main.e64eb2bd.chunk.js"
+  },
+  {
+    "revision": "6848653ac989f6d300a2",
+    "url": "./static/js/runtime-main.26353912.js"
+  }
+]);
